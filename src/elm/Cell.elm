fc9890d3af127fbd71528868a1abefa07a2064a2
David Corwin <dave@launchpadlab.com>
2016-12-05 21:32:56 -0600

    fix shift selection

--- src/elm/Cell.elm
+++ src/elm/Cell.elm
@@ -20,30 +20,16 @@ equal cell1 cell2 =
 
 min : Cell -> Cell -> Cell
 min cell1 cell2 =
-    if (row cell1) < (row cell2) then
-        cell1
-    else if (row cell2) < (row cell1) then
-        cell2
-    else if (col cell1) < (col cell2) then
-        cell1
-    else if (col cell2) < (col cell1) then
-        cell2
-    else
-        cell1
+    ( B.min (row cell1) (row cell2)
+    , B.min (col cell1) (col cell2)
+    )
 
 
 max : Cell -> Cell -> Cell
 max cell1 cell2 =
-    if (row cell1) < (row cell2) then
-        cell2
-    else if (row cell2) < (row cell1) then
-        cell1
-    else if (col cell1) < (col cell2) then
-        cell2
-    else if (col cell2) < (col cell1) then
-        cell1
-    else
-        cell1
+    ( B.max (row cell1) (row cell2)
+    , B.max (col cell1) (col cell2)
+    )
 
 
 row : Cell -> Int
